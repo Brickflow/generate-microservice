99cacbbacf8affa8c3161619f9f6748b27337cd2
David Judik <judik.david@gmail.com>
2014-11-28 16:33:42 +0100

    initial commit

---
+++ app/index.js
@@ -0,0 +1,106 @@
+'use strict';
+
+var path = require('path');
+var npmName = require('npm-name');
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+  init: function() {
+    this.log(this.yeoman);
+  },
+
+  askForModuleName: function () {
+    var done = this.async();
+
+    var prompts = [{
+      name: 'name',
+      message: 'Module Name',
+      default: path.basename(process.cwd()),
+    }, {
+      type: 'confirm',
+      name: 'pkgName',
+      message: 'The name above already exists on npm, choose another?',
+      default: true,
+               when: function(answers) {
+                 var done = this.async();
+
+                 npmName(answers.name, function (err, available) {
+                   if (!available) {
+                     done(true);
+                   }
+
+                   done(false);
+                 });
+               }
+    }];
+
+    this.prompt(prompts, function (props) {
+      if (props.pkgName) {
+        return this.askForModuleName();
+      }
+
+      this.slugname = this._.slugify(props.name);
+      this.safeSlugname = this.slugname.replace(
+          /-+([a-zA-Z0-9])/g,
+          function (g) { return g[1].toUpperCase(); }
+          );
+
+      done();
+    }.bind(this));
+  },
+
+  askFor: function () {
+    var cb = this.async();
+
+    var prompts = [{
+      name: 'description',
+      message: 'Description',
+      default: 'The best module ever.'
+    }, {
+      name: 'license',
+      message: 'License',
+      default: 'MIT'
+    }, {
+      name: 'githubUsername',
+      message: 'GitHub username'
+    }, {
+      name: 'authorName',
+      message: 'Author\'s Name'
+    }, {
+      name: 'authorEmail',
+      message: 'Author\'s Email'
+    }, {
+      name: 'keywords',
+      message: 'Key your keywords (comma to split)'
+    }];
+
+    this.currentYear = (new Date()).getFullYear();
+
+    this.prompt(prompts, function (props) {
+      if(props.githubUsername){
+        this.repoUrl = 'https://github.com/' + props.githubUsername + '/' + this.slugname;
+      } else {
+        this.repoUrl = 'user/repo';
+      }
+
+      if (!props.homepage) {
+        props.homepage = this.repoUrl;
+      }
+
+      this.keywords = props.keywords.split(',');
+
+      this.props = props;
+
+      cb();
+    }.bind(this));
+  },
+
+  app: function () {
+      this.config.save();
+      this.copy('jshintrc', '.jshintrc');
+      this.copy('gitignore', '.gitignore');
+
+      this.template('_README.md', 'README.md');
+      this.template('_package.json', 'package.json');
+  },
+});
